d3be86d65720e36a7816225b570baef06f34e39f
Emmanuel Bandres <35436932+ebandres@users.noreply.github.com>
2021-06-22 16:00:13 -0400

    main for the other problems (without manhattan)

--- Problems/TopSpin/TopSpin_12_4/main.cpp
+++ Problems/TopSpin/TopSpin_12_4/main.cpp
@@ -2,6 +2,8 @@
 #include <set>
 #include <map>
 #include <iostream>
+#include <cstring>
+#include <fstream>
 #include <chrono>
 #include <bits/stdc++.h>
 #include <signal.h>
@@ -18,13 +20,162 @@ using namespace std::chrono;
 
 unsigned (*heuristic) (state_t);
 
+ifstream myFile;
+ofstream resultFile;
+
+void signal_callback_handler(int signum) {
+	if (myFile.is_open())
+	{
+		myFile.close();
+	}
+
+	if (resultFile.is_open())
+	{
+		resultFile << "\nProgram terminated by user" << endl;
+		resultFile.close();
+	}
+	
+	exit(signum);
+}
+
 int main(void) {
+	signal(SIGINT, signal_callback_handler);
 
 	vector<int> result;
+	int result_a;
 	state_t *init = new state_t;
 	char str[300 + 1]; // Para la prueba se pone una representación del estado en string
 	ssize_t nchars;
 
+	cout << "Select algorithm:\n\t1 - A*\n\t2 - IDA*" << endl;
+	if (fgets(str, sizeof(str), stdin) == NULL) {
+		cout << "No input" << endl;
+		return 1;
+	}
+
+	int alg = atoi(str); // For later
+
+	cout << "Input option:\n\t1 - PDB" << endl;
+	if (fgets(str, sizeof(str), stdin) == NULL) {
+		cout << "No input" << endl;
+		return 1; 
+	}
+	cout << endl;
+
+	int selection = atoi(str);
+
+	switch (selection)
+	{
+	case 1:
+		set_pdb();
+		heuristic = pdb_add;
+		break;
+	}
+
+	string line;
+	myFile.open("input.txt");
+
+	if (myFile.is_open()) 
+	{
+		resultFile.open("results.txt", ios::out | ios::trunc);
+
+		resultFile << "These results were obtained using ";
+
+		switch (alg)
+		{
+		case 1:
+			resultFile << "A* with ";
+			break;
+		
+		case 2:
+			resultFile << "IDA* with ";
+			break;
+		}
+
+		switch (selection)
+		{
+		case 1:
+			resultFile << "Additive PDB Heuristic" << endl;
+			break;
+		}
+		
+
+		while ( getline(myFile, line) )
+    	{
+			strcpy(str, line.c_str());
+			nchars = read_state(str, init);
+			if (nchars <= 0) {
+				cout << "Invalid state" << endl;
+				return 2; 
+			}
+			cout << "\nstate:" << endl;
+			print_state(stdout, init);
+			cout << endl;
+
+			resultFile << "\nstate:\n" << str << endl;
+
+			int64_t generatedNodes = 1;
+
+			time_point<high_resolution_clock> start;
+
+			try
+			{
+				switch (alg)
+				{
+				case 1:
+					start = high_resolution_clock::now();
+					result_a = a_search(init, heuristic, true, generatedNodes, start);
+					break;
+				
+				case 2:
+					start = high_resolution_clock::now();
+					result = ida_search(init, heuristic, true, generatedNodes, start);
+					break;
+				}
+			}
+			catch(int e)
+			{
+				if (e == 3)
+				{
+					cout << "Time limit reached" << endl;
+					resultFile << "Time limit reached" << endl;
+				}
+				else
+				{
+					cout << "Error code " << e << endl;
+					resultFile << "Error code " << e << endl;
+				}
+				result.clear();
+				result_a = 0;
+			}
+			
+
+			auto stop = high_resolution_clock::now();
+
+			duration<double> total = stop - start;
+			cout << "time: " << total.count() << " s\t";
+			resultFile << "time: " << total.count() << " s\t";
+			switch (alg)
+			{
+			case 1:
+				cout << "solution cost: " << result_a << endl;
+				resultFile << "solution cost: " << result_a << endl;
+				break;
+			
+			case 2:
+				cout << "solution length: " << result.size() << endl;
+				resultFile << "solution length: " << result.size() << endl;
+				break;
+			}
+			cout << "generated nodes: " << generatedNodes << endl;
+			resultFile << "generated nodes: " << generatedNodes << endl;
+    	}
+    	resultFile.close();
+		myFile.close();
+	}
+
+
+	/*
 	cout << "Input initial state:\n";
 	if (fgets(str, sizeof(str), stdin) == NULL) {
 		cout << "No input" << endl;
@@ -32,31 +183,14 @@ int main(void) {
 	}
 	// Convert the string to a state
 	nchars = read_state(str, init);
+	
 	print_state(stdout, init);
 	if (nchars <= 0) {
 		cout << "Invalid state" << endl;
 		return 2; 
 	}
 
-	cout << "Input option:\n\t1 Manhattan\n\t2 PDB" << endl;
-	if (fgets(str, sizeof(str), stdin) == NULL) {
-		cout << "No input" << endl;
-		return 1; 
-	}
 
-	int selection = atoi(str);
-
-	switch (selection)
-	{
-	case 1:
-		set_manhattan();
-		heuristic = manhattan;
-		break;
-	case 2:
-		set_pdb();
-		heuristic = pdb_add;
-		break;
-	}
 
 	int64_t generatedNodes = 1;
 	auto start = high_resolution_clock::now();
@@ -67,4 +201,5 @@ int main(void) {
 	duration<double> total = stop - start;
 	cout << "time: " << total.count() << " s\t" << "solution length: " << result.size() << endl;
 	cout << generatedNodes << endl;
+	*/
 }
